efc33dac26791a1b5734a4ddbfa28f27105bc2af
johnbean393 <johnbean393@gmail.com>
2025-01-21 20:33:06 +0800

    Initial Commit

---
+++ Facted/FactedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactedApp.swift
+//  Facted
+//
+//  Created by John Bean on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FactedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FactedTests/FactedTests.swift
@@ -0,0 +1,17 @@
+//
+//  FactedTests.swift
+//  FactedTests
+//
+//  Created by John Bean on 1/21/25.
+//
+
+import Testing
+@testable import Facted
+
+struct FactedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
